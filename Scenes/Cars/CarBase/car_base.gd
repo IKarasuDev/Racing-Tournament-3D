c48a983ca8adc0258b25bd324365e88f28ea5685
kbradev <ltovar.dev@gmail.com>
2026-03-05 12:55:51 -0600

    add new cars

--- Scenes/Cars/CarBase/car_base.gd
+++ Scenes/Cars/CarBase/car_base.gd
@@ -4,10 +4,23 @@ extends VehicleBody3D
 @export var max_brake_force := 100.0
 @export var max_steering_angle := 0.5
 
+# mesh editable desde el inspector del VehicleBody3D
+@export var car_mesh: Mesh:
+	set(value):
+		car_mesh = value
+		if is_inside_tree():
+			$CarMesh.mesh = value
+
 @onready var fl = $FrontLeft
 @onready var fr = $FrontRight
 @onready var rl = $RearLeft
 @onready var rr = $RearRight
+
+
+func _ready():
+	if car_mesh:
+		$CarMesh.mesh = car_mesh
+
 
 func _physics_process(delta):
 	var forward_input = Input.get_axis("S", "W")
